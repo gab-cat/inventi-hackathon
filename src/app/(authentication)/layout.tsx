82b5f713397f6a6ff3a489ca4ee29270e11e59bc
martin <meatole.cs@gmail.com>
2025-09-16 20:18:00 +0800

    feat: signin and signup page

---
+++ src/app/(authentication)/layout.tsx
@@ -0,0 +1,42 @@
+import { ArrowLeft } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function AuthLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className='min-h-screen bg-gray-50 flex flex-col'>
+            {/* Header */}
+            <div className='absolute lg:p-10 p-4'>
+                <Link
+                    href='/'
+                    className='inline-flex items-center gap-2 text-neutral-600 font-semibold transition-colors text-sm md:text-sm'
+                >
+                    <ArrowLeft className='w-4 h-4' />
+                    Back to Home
+                </Link>
+            </div>
+
+            {/* Main Content */}
+            <div className='flex-1 flex items-center justify-center px-4 min-h-screen'>
+                <div className='w-full max-w-md'>
+                    {/* Logo */}
+                    <div className='text-center mb-2'>
+                        <Image
+                            src='/sophons-logo-dark.png'
+                            alt='Logo'
+                            width={500}
+                            height={500}
+                            className='mx-auto h-12 w-auto mb-5'
+                        />
+                    </div>
+
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+}
